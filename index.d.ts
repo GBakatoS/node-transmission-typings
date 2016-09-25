ab3abcbda21b9f0a545b52bcba416124cdc49cb4
yusiswtor <ronnie@rl-web.no>
2016-09-25 19:53:15 +0200

    Namespace

--- index.d.ts
+++ index.d.ts
@@ -1,200 +1,203 @@
 // Type definitions for node-transmission
 // Project: https://github.com/FLYBYME/node-transmission
 // Definitions by: Ronnie Laugen <https://github.com/yusijs>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Definitions: https://github.com/yusijs/node-transmission-typings
 
 declare module "transmission" {
 
 
-        interface setupOptions { host?: string, port?: number, username?: string, password?: string, url?: string }
-        interface options {
-                [propName: string]: string
+        namespace Transmission {
+
+                interface setupOptions { host?: string, port?: number, username?: string, password?: string, url?: string }
+                interface options {
+                        [propName: string]: string
+                }
+
+                interface setTorrentOptions {
+                        bandwidthPriority?: string
+                        downloadLimit?: number
+                        downloadLimited?: boolean
+                        'files-wanted'?: string[]
+                        'files-unwanted'?: string[]
+                        honorsSessionLimits?: boolean
+                        ids?: number[]
+                        location?: string
+                        'peer-limit'?: number
+                        'priority-high'?: number[]
+                        'priority-low'?: number[]
+                        'priority-normal'?: number[]
+                        seedRatioLimit?: number
+                        seedRatioMode?: number
+                        uploadLimit?: number
+                        uploadLimited?: boolean
+                }
+
+                interface setSessionOptions {
+                        'start-added-torrents': boolean
+                        'alt-speed-down': boolean
+                        'alt-speed-enabled': boolean
+                        'alt-speed-time-begin': boolean
+                        'alt-speed-time-enabled': boolean
+                        'alt-speed-time-end': boolean
+                        'alt-speed-time-day': boolean
+                        'alt-speed-up': boolean
+                        'blocklist-enabled': boolean
+                        'dht-enabled': boolean
+                        encryption: boolean
+                        'download-dir': boolean
+                        'peer-limit-global': boolean
+                        'peer-limit-per-torrent': boolean
+                        'pex-enabled': boolean
+                        'peer-port': boolean
+                        'peer-port-random-on-start': boolean
+                        'port-forwarding-enabled': boolean
+                        seedRatioLimit: boolean
+                        seedRatioLimited: boolean
+                        'speed-limit-down': boolean
+                        'speed-limit-down-enabled': boolean
+                        'speed-limit-up': boolean
+                        'speed-limit-up-enabled': boolean
+                }
+
+                interface peersFrom {
+                        "fromCache": number,
+                        "fromDht": number,
+                        "fromIncoming": number,
+                        "fromLpd": number,
+                        "fromLtep": number,
+                        "fromPex": number,
+                        "fromTracker": number
+                }
+
+                interface torrentInfo {
+                        activityDate: number
+                        addedDate: number
+                        bandwidthPriority: number
+                        comment: string
+                        corruptEver: number
+                        creator: string
+                        dateCreated: number
+                        desiredAvailable: number
+                        doneDate: number
+                        downloadDir: string
+                        downloadedEver: number
+                        downloadLimit: number
+                        downloadLimited: boolean
+                        error: number
+                        errorString: string
+                        eta: number
+                        etaIdle: number
+                        files: files[]
+                        fileStats: fileStats[]
+                        hashString: string
+                        haveUnchecked: number
+                        haveValid: number
+                        honorsSessionLimits: boolean
+                        id: number
+                        isFinished: boolean
+                        isPrivate: boolean
+                        isStalled: boolean
+                        leftUntilDone: number
+                        magnetLink: string
+                        manualAnnounceTime: number
+                        maxConnectedPeers: number
+                        metadataPercentComplete: number
+                        name: string
+                        "peer-limit": number
+                        peers: any[]
+                        peersConnected: number
+                        peersFrom: peersFrom
+                        peersGettingFromUs: number
+                        peersSendingToUs: number
+                        percentDone: number
+                        pieces: string
+                        pieceCount: number
+                        pieceSize: number
+                        'priorities': number[]
+                        'rateDownload': number
+                        'rateUpload': number
+                        'recheckProgress': string
+                        'seedIdleLimit': number
+                        'seedIdleMode': string
+                        'seedRatioLimit': number
+                        'seedRatioMode': string
+                        'sizeWhenDone': number
+                        'startDate': string
+                        'status': string
+                        'trackers': trackers[]
+                        'trackerStats': trackerStats[]
+                        'totalSize': number
+                        'torrentFile': string
+                        'uploadedEver': number
+                        'uploadLimit': number
+                        'uploadLimited': string
+                        'uploadRatio': number
+                        'wanted': number[]
+                        'webseeds': any[]
+                        'webseedsSendingToUs': number
+                }
+
+                interface fileStats {
+                        bytesCompleted: number
+                        priority: number
+                        wanted: boolean
+                }
+
+                interface files {
+                        bytesCompleted: number
+                        length: number
+                        name: string
+                }
+
+                interface trackers {
+                        announce: string
+                        id: number
+                        scrape: string
+                        tier: number
+                }
+
+                interface trackerStats {
+                        "announce": string
+                        "announceState": number,
+                        "downloadCount": number,
+                        "hasAnnounced": boolean,
+                        "hasScraped": boolean,
+                        "host": string,
+                        "id": number,
+                        "isBackup": boolean,
+                        "lastAnnouncePeerCount": number,
+                        "lastAnnounceResult": string,
+                        "lastAnnounceStartTime": number,
+                        "lastAnnounceSucceeded": boolean,
+                        "lastAnnounceTime": number,
+                        "lastAnnounceTimedOut": boolean,
+                        "lastScrapeResult": string,
+                        "lastScrapeStartTime": number,
+                        "lastScrapeSucceeded": boolean,
+                        "lastScrapeTime": number,
+                        "lastScrapeTimedOut": number,
+                        "leecherCount": number,
+                        "nextAnnounceTime": number,
+                        "nextScrapeTime": number,
+                        "scrape": string,
+                        "scrapeState": number,
+                        "seederCount": number,
+                        "tier": number
+                }
+
+                interface results {
+                        torrents?: torrentInfo[],
+                        id?: number
+                        name?: string
+                        hashString?: string
+                        [propName: string]: any
+                }
         }
 
-        interface setTorrentOptions {
-                bandwidthPriority: string
-                downloadLimit: number
-                downloadLimited: boolean
-                'files-wanted': string[]
-                'files-unwanted': string[]
-                honorsSessionLimits: boolean
-                ids: number[]
-                location: string
-                'peer-limit': number
-                'priority-high': number[]
-                'priority-low': number[]
-                'priority-normal': number[]
-                seedRatioLimit: number
-                seedRatioMode: number
-                uploadLimit: number
-                uploadLimited: boolean
-        }
-
-        interface setSessionOptions {
-                'start-added-torrents': boolean
-                'alt-speed-down': boolean
-                'alt-speed-enabled': boolean
-                'alt-speed-time-begin': boolean
-                'alt-speed-time-enabled': boolean
-                'alt-speed-time-end': boolean
-                'alt-speed-time-day': boolean
-                'alt-speed-up': boolean
-                'blocklist-enabled': boolean
-                'dht-enabled': boolean
-                encryption: boolean
-                'download-dir': boolean
-                'peer-limit-global': boolean
-                'peer-limit-per-torrent': boolean
-                'pex-enabled': boolean
-                'peer-port': boolean
-                'peer-port-random-on-start': boolean
-                'port-forwarding-enabled': boolean
-                seedRatioLimit: boolean
-                seedRatioLimited: boolean
-                'speed-limit-down': boolean
-                'speed-limit-down-enabled': boolean
-                'speed-limit-up': boolean
-                'speed-limit-up-enabled': boolean
-        }
-
-        interface peersFrom {
-                "fromCache": number,
-                "fromDht": number,
-                "fromIncoming": number,
-                "fromLpd": number,
-                "fromLtep": number,
-                "fromPex": number,
-                "fromTracker": number
-        }
-
-        interface torrentInfo {
-                activityDate: number
-                addedDate: number
-                bandwidthPriority: number
-                comment: string
-                corruptEver: number
-                creator: string
-                dateCreated: number
-                desiredAvailable: number
-                doneDate: number
-                downloadDir: string
-                downloadedEver: number
-                downloadLimit: number
-                downloadLimited: boolean
-                error: number
-                errorString: string
-                eta: number
-                etaIdle: number
-                files: files[]
-                fileStats: fileStats[]
-                hashString: string
-                haveUnchecked: number
-                haveValid: number
-                honorsSessionLimits: boolean
-                id: number
-                isFinished: boolean
-                isPrivate: boolean
-                isStalled: boolean
-                leftUntilDone: number
-                magnetLink: string
-                manualAnnounceTime: number
-                maxConnectedPeers: number
-                metadataPercentComplete: number
-                name: string
-                "peer-limit": number
-                peers: any[]
-                peersConnected: number
-                peersFrom: peersFrom
-                peersGettingFromUs: number
-                peersSendingToUs: number
-                percentDone: number
-                pieces: string
-                pieceCount: number
-                pieceSize: number
-                'priorities': number[]
-                'rateDownload': number
-                'rateUpload': number
-                'recheckProgress': string
-                'seedIdleLimit': number
-                'seedIdleMode': string
-                'seedRatioLimit': number
-                'seedRatioMode': string
-                'sizeWhenDone': number
-                'startDate': string
-                'status': string
-                'trackers': trackers[]
-                'trackerStats': trackerStats[]
-                'totalSize': number
-                'torrentFile': string
-                'uploadedEver': number
-                'uploadLimit': number
-                'uploadLimited': string
-                'uploadRatio': number
-                'wanted': number[]
-                'webseeds': any[]
-                'webseedsSendingToUs': number
-        }
-
-        interface fileStats {
-                bytesCompleted: number
-                priority: number
-                wanted: boolean
-        }
-
-        interface files {
-                bytesCompleted: number
-                length: number
-                name: string
-        }
-
-        interface trackers {
-                announce: string
-                id: number
-                scrape: string
-                tier: number
-        }
-
-        interface trackerStats {
-                "announce": string
-                "announceState": number,
-                "downloadCount": number,
-                "hasAnnounced": boolean,
-                "hasScraped": boolean,
-                "host": string,
-                "id": number,
-                "isBackup": boolean,
-                "lastAnnouncePeerCount": number,
-                "lastAnnounceResult": string,
-                "lastAnnounceStartTime": number,
-                "lastAnnounceSucceeded": boolean,
-                "lastAnnounceTime": number,
-                "lastAnnounceTimedOut": boolean,
-                "lastScrapeResult": string,
-                "lastScrapeStartTime": number,
-                "lastScrapeSucceeded": boolean,
-                "lastScrapeTime": number,
-                "lastScrapeTimedOut": number,
-                "leecherCount": number,
-                "nextAnnounceTime": number,
-                "nextScrapeTime": number,
-                "scrape": string,
-                "scrapeState": number,
-                "seederCount": number,
-                "tier": number
-        }
-
-        interface results {
-                torrents?: torrentInfo[],
-                id?: number
-                name?: string
-                hashString?: string
-                [propName: string]: any
-        }
-
-        type callback = (err: Error, results: results) => void;
+        type callback = (err: Error, results: Transmission.results) => void;
 
         class Transmission {
-                constructor(setupOptions: setupOptions)
+                constructor(setupOptions: Transmission.setupOptions)
 
                 /**
                  * Set properties for a torrent. You must provide one or more ids.
@@ -202,7 +205,7 @@ declare module "transmission" {
                  * @param ids an array of torrent IDs to set properties for
                  * @param options options as defined by the RCP spec @ https://trac.transmissionbt.com/browser/trunk/extras/rpc-spec.txt
                  */
-                set(ids: number[] | number, options: setTorrentOptions, callback: callback): void;
+                set(ids: number[] | number, options: Transmission.setTorrentOptions, callback: callback): void;
 
                 /**
                  * Add a torrent from a local file
@@ -211,7 +214,7 @@ declare module "transmission" {
                  * @param options (optional) options as defined by the RCP spec @ https://trac.transmissionbt.com/browser/trunk/extras/rpc-spec.txt
                  */
 
-                addFile(filePath: string, options: options, callback: callback): void;
+                addFile(filePath: string, options: Transmission.options, callback: callback): void;
                 addFile(filePath: string, callback: callback): void;
 
 
@@ -221,7 +224,7 @@ declare module "transmission" {
                  * @param filePath string describing where the file is located
                  * @param options (optional) options as defined by the RCP spec @ https://trac.transmissionbt.com/browser/trunk/extras/rpc-spec.txt
                  */
-                addUrl(url: string, options: options, callback: callback): void;
+                addUrl(url: string, options: Transmission.options, callback: callback): void;
                 addUrl(url: string, callback: callback): void;
 
                 /**
@@ -298,7 +301,7 @@ declare module "transmission" {
                  * According to the rpc-spec, transmission will not respond a success argument. Only error.
                  * @param options (optional) options as defined by the RCP spec @ https://trac.transmissionbt.com/browser/trunk/extras/rpc-spec.txt
                  */
-                session(options: setSessionOptions, callback: callback): void;
+                session(options: Transmission.setSessionOptions, callback: callback): void;
 
                 /**
                  * Get session statistics
